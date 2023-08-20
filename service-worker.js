44c82634df45770e083c14f482bb6b4165110158
ajstars1 <ayushjamwal10@gmail.com>
2023-08-21 03:56:04 +0530

    v-0.3

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,35 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: "ON",
-    });
+const shortsURL = 'https://www.youtube.com/shorts/';
+let extensionStatus = 'ON';
+
+function toggleExtensionStatus(tabId) {
+  extensionStatus = extensionStatus === 'ON' ? 'OFF' : 'ON';
+  chrome.action.setBadgeText({
+    tabId,
+    text: extensionStatus,
   });
-
-const shorts = 'https://www.youtube.com/shorts/'
-
-let Status = "ON";
-chrome.action.onClicked.addListener(async (tab) => {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-    Status = nextState;
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
 }
-);
 
+// Listen for the extension icon (action) being clicked
+chrome.action.onClicked.addListener(async (tab) => {
+  toggleExtensionStatus(tab.id);
+});
+
+// Listen for changes in navigation history state (page changes)
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-    if (Status === "ON" && details.url.startsWith(shorts)) {
-        chrome.scripting.executeScript({
-            target : {tabId : details.tabId},
-            func : (()=>{
-                window.location.href = "https://www.youtube.com/";
-            }),
-          });
-      } 
-  
+  if (extensionStatus === 'ON' && details.url.startsWith(shortsURL)) {
+    // Inject a script to redirect the page to the main YouTube homepage
+    chrome.scripting.executeScript({
+      target: { tabId: details.tabId },
+      function: () => {
+        window.location.href = 'https://www.youtube.com/';
+      },
+    });
   }
-);
+});
+
+// Listen for the extension being installed or updated
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: extensionStatus,
+  });
+});
